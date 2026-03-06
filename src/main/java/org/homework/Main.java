13aeea70318484b36f1f73ef1121e6444ff0a91d
Pilyaeva Polina <pilyaevapolina@gmail.com>
2026-03-06 17:12:24 +0300

    Rename папки

--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -1,4 +1,4 @@
-package org.tic_tac_toe;
+package org.homework;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
