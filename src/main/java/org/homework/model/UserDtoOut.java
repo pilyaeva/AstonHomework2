6938c614ba6e08b5c1955b28dff5c56830d1eaa6
Pilyaeva Polina <PilyaevaPolina@yandex.ru>
2026-03-22 13:14:53 +0300

    Убрала комментарии

--- src/main/java/org/homework/model/UserDtoOut.java
+++ src/main/java/org/homework/model/UserDtoOut.java
@@ -2,9 +2,6 @@ package org.homework.model;
 
 import java.time.LocalDateTime;
 
-/**
- * Пользователь.
- */
 public class UserDtoOut {
     private final Long id;
     private final String name;
@@ -12,15 +9,6 @@ public class UserDtoOut {
     private final Integer age;
     private final LocalDateTime createdAt;
 
-    /**
-     * Конструктор.
-     *
-     * @param id        Идентификатор пользователя
-     * @param name      Имя пользователя
-     * @param email     Email пользователя
-     * @param age       Возраст пользователя
-     * @param createdAt Дата создания
-     */
     public UserDtoOut(Long id, String name, String email, Integer age, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
